7171e04fabc10693d47aa0d12ea4ca2823743adf
Javier Carrillo <javier.60207@gmail.com>
2022-08-12 12:44:12 -0700

    very first commit

---
+++ part1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Mensaje  from './Mensaje';
+
+const Description = () => {
+  return (
+  <p>Esta es la descripción del curso fullstack bootcamp</p>
+  )
+}
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje color='green' message='Estamos trabajando'/>
+      <Mensaje color='red' message='En un curso'/>
+      <Mensaje color='yellow' message='de React'/>
+      <Description />
+    </div>
+  );
+}
+
+export default App;
