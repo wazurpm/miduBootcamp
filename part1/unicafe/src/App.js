aa6898529cc6998407c2defe47a596a5a308ce45
Javier Carrillo <javier.60207@gmail.com>
2022-10-10 15:31:52 -0700

    hecho el ejercicio de unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [statistics, setStatistics] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  })
+
+  const feedback = (action) => {
+      if(action === 'good'){
+        return () => {setStatistics((prevSatistics) => {
+          const newStatistics = {
+            ...prevSatistics,
+            good: statistics.good + 1
+          }
+          return newStatistics
+        })};
+      } else if(action === 'neutral'){
+        return () => {setStatistics((prevSatistics) => {
+          const newStatistics = {
+            ...prevSatistics,
+            neutral: statistics.neutral + 1
+          }
+          return newStatistics
+        })};
+      } else if(action === 'bad'){
+        return () => {setStatistics((prevSatistics) => {
+          const newStatistics = {
+            ...prevSatistics,
+            bad: statistics.bad + 1
+          }
+          return newStatistics
+        })};
+      }
+    }
+
+  return (<>
+    <h1>give feedback</h1>
+    <div>
+      <button onClick={feedback('good')}>good</button>
+      <button onClick={feedback('neutral')}>neutral</button>
+      <button onClick={feedback('bad')}>bad</button>
     </div>
-  );
+    <h1>statistics</h1>
+    <p>good {statistics.good}</p>
+    <p>neutral {statistics.neutral}</p>
+    <p>bad {statistics.bad}</p>
+  </>);
 }
 
 export default App;
