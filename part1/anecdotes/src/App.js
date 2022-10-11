350a192cc7ab8a0f91ecef59d43c03e0578807bc
Javier Carrillo <javier.60207@gmail.com>
2022-10-11 15:02:39 -0700

    agregado el contador de votos

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+const initialPoints = anecdotes.map((value) => {return value = 0});
+
+const Anecdote = (props) => {
+  return (<div>
+  <h2>{anecdotes[props.selection]}</h2>
+  <p>has {props.votes} votes</p>
+  </div>);
+}
+
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState([...initialPoints]);
+
+  const nextAnecdote = () => {
+    const num = Math.floor(Math.random() * (anecdotes.length));
+    setSelected(num);
+    return num;
+  }
+
+  const vote = () => {
+    return setPoints((prevPoints) => {
+      const newPoints = [...prevPoints];
+      newPoints[selected] = newPoints[selected] + 1;
+      return newPoints;
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Anecdote selection={selected} votes={points[selected]}/>
+      <button onClick={vote}>vote</button>
+      <button onClick={nextAnecdote}>next anecdote</button>
     </div>
   );
 }
